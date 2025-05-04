1c9e5a1196207b26ab3aaa2a8c73dc3bf205bb9d
ankur24s <ankursahu64@gmail.com>
2025-05-04 11:25:14 +0530

    added codes

--- models/gold_airbnb/fact_reviews.sql
+++ models/gold_airbnb/fact_reviews.sql
@@ -1,16 +1,12 @@
 {{
-  config(
+    config(
     materialized = 'incremental',
-    unique_key = ['listing_id','reviewer_name','review_date'],
-    cluster_by = ['review_date'],  
-    incremental_strategy = 'merge',
-	  incremental_predicates 	= [
-      "DBT_INTERNAL_DEST.review_date > dateadd(year, -4, current_date)"
-      ]
-  )
+    incremental_strategy='append',
+    on_schema_change = 'fail'
+    )
 }}
 SELECT * FROM {{ ref('stg_reviews') }}
 WHERE review_text is not null
 {% if is_incremental() %}
-and review_date > (select DATEADD('day', -7, max(review_date)) from {{this}})
+and review_date > (select max(review_date) from {{this}})
 {% endif %}
